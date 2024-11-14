6b102a66f819e73b598e85e510a79fc1a855208c
Alan Abdiel <Alan Abdiel@LAPTOP-DA4AJN7D>
2024-11-14 17:37:43 -0600

    Agregar archivos de proyecto.

---
+++ Practica 2 Hola mundo/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Practica_2_Hola_mundo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Showcard Gothic", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(92, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(186, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Hola, Mundo!";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Showcard Gothic", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(105, 143);
+            label2.Name = "label2";
+            label2.Size = new Size(161, 18);
+            label2.TabIndex = 1;
+            label2.Text = "Cuál es tu nombre?";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Showcard Gothic", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(105, 249);
+            label3.Name = "label3";
+            label3.Size = new Size(194, 18);
+            label3.TabIndex = 2;
+            label3.Text = "Hola, alguien especial!";
+            label3.Click += label3_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Showcard Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(105, 191);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(204, 33);
+            textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Showcard Gothic", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(417, 169);
+            button1.Name = "button1";
+            button1.Size = new Size(182, 56);
+            button1.TabIndex = 4;
+            button1.Text = "Diga \"Hola\"";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "ola (de mar)";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private Button button1;
+    }
+}
